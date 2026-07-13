c1a5ec9477679e9c5d86f42577d5c3a2821b860a
Ahmadgaber3 <ans354093@gmail.com>
2026-07-13 23:38:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,22 @@
-const CACHE_NAME = 'abu-farouk-v2';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'abu-farouk-v1';
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
   './images/logooo.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(ASSETS);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
     })
   );
 });
